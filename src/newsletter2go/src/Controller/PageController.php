ec98ee9884403c8f5d54cc8687783c809d752c3c
valthebald <valery@wisetrout.com>
2017-04-07 17:07:13 +0300

    Move callbacks from hook_menu() [WIP]

---
+++ src/newsletter2go/src/Controller/PageController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Drupal\newsletter2go\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class PageController.
+ *
+ * @package Drupal\newsletter2go\Controller
+ */
+class PageController extends ControllerBase {
+
+  /**
+   * Widgetpreview.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function widgetPreview() {
+    return [
+      '#type' => 'markup',
+      '#markup' => urldecode($_GET['widget']),
+    ];
+  }
+
+  /**
+   * Process instance authorization.
+   */
+  public function apiGo() {
+    require_once __DIR__ . '/newsletter2goApi.php';
+    $instance = Newsletter2GoApi::getInstance();
+    $instance->processRequest($_SERVER['PHP_AUTH_USER'], $_GET, $_POST);
+  }
+
+  /**
+   * Process callback.
+   */
+  public function goCallback() {
+    require_once __DIR__ . '/newsletter2goCallback.php';
+    $instance = Newsletter2GoCallback::getInstance();
+    $instance->processCallback($_POST);
+  }
+
+
+  /**
+   * Request newsletter2go page.
+   */
+  function subscribe()
+  {
+    $notFound = false;
+    $noValidEmail = false;
+    $attributes = variable_get('newsletter2go_fields');
+    $requiredFields = variable_get('newsletter2go_required');
+    $texts = variable_get('newsletter2go_texts');
+    $post = array();
+    foreach ($attributes as $k => $v) {
+      if (!empty($requiredFields[$k]) && empty($_POST[$k])) {
+        $notFound = true;
+        break;
+      }
+
+      if ($k == 'email') {
+        if (!filter_var($_POST[$k], FILTER_VALIDATE_EMAIL)) {
+          $noValidEmail = true;
+        }
+      }
+
+      $post[$k] = $_POST[$k];
+    }
+
+    if ($notFound) {
+      drupal_json_output(array('success' => 0, 'message' => $texts['failureRequired']));
+      drupal_exit();
+    }
+
+    if ($noValidEmail) {
+      drupal_json_output(array('success' => 0, 'message' => $texts['failureEmail']));
+      drupal_exit();
+    }
+
+    $post['key'] = variable_get('newsletter2go_apikey');
+    $post['doicode'] = variable_get('newsletter2go_doicode');
+    $response = executeN2Go('create/recipient', $post);
+
+    $result = array('success' => $response['success']);
+    if (!$response) {
+      $result['message'] = $texts['failureEmail'];
+    } else {
+      switch ($response['status']) {
+        case 200:
+          $result['message'] = $texts['success'];
+          break;
+        case 441:
+          $result['message'] = $texts['failureSubsc'];
+          break;
+        case 434:
+        case 429:
+          $result['message'] = $texts['failureEmail'];
+          break;
+        default:
+          $result['message'] = $texts['failureGeneral'];
+          break;
+      }
+    }
+
+    drupal_json_output($result);
+    drupal_exit();
+  }
+
+  /**
+   * This function sets widgetStyleConfig to default value
+   */
+  function resetStyles() {
+    $style = $_POST['style'];
+    variable_set('newsletter2go_widgetStyleConfig', $style);
+    print 'success';
+    drupal_exit();
+  }
+
+}
